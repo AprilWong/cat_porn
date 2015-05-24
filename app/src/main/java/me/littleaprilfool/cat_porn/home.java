95c8fd39fd01de6d7ab4643e8b507f1124d67bdd
AprilWong <aprilwang.zju@gmail.com>
2015-05-25 00:59:13 +0800

    create own camera

--- app/src/main/java/me/littleaprilfool/cat_porn/home.java
+++ app/src/main/java/me/littleaprilfool/cat_porn/home.java
@@ -1,38 +1,119 @@
 package me.littleaprilfool.cat_porn;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.hardware.Camera;
+
+import java.io.IOException;
+
+public class home extends Activity {
+
+    public boolean if_connect;
+    public boolean if_record;
 
 
-public class home extends ActionBarActivity {
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private Camera mCamera;
+    private Button cButton;
+    private Button rButton;
+
+    private void InitCamera(){
+        Log.d("linc","start camera");
+        try{
+            mCamera = Camera.open();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStart(){
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        Log.d("linc","start");
+        InitCamera();
+        super.onStart();
+        try{
+            if (mCamera != null){
+                Log.d("linc","setdisplay");
+                mCamera.setPreviewDisplay(mSurfaceHolder);
+                mCamera.setDisplayOrientation(90);
+                mCamera.startPreview();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        try {
+            if (mCamera != null) {
+                mCamera.setPreviewCallback(null);
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
+        mSurfaceView = (SurfaceView)findViewById(R.id.surface);
+        cButton = (Button)findViewById(R.id.connect_button);
+        rButton = (Button)findViewById(R.id.record_button);
+        if_connect = false;
+        if_record = false;
+        rButton.setEnabled(false);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Log.d("linc", "oncreate");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        rButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(if_record){
+                    if_record = false;
+                    rButton.setText("start");
+                }
+                else{
+                    if_record = true;
+                    rButton.setText("stop");
+                }
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+        cButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (if_connect){
+                    if_connect = false;
+                    if_record = false;
+                    rButton.setEnabled(false);
+                    rButton.setText("start");
+                    cButton.setText("connect");
+                }
+                else{
+                    if_connect = true;
+                    rButton.setEnabled(true);
+                    cButton.setText("disconnect");
+                }
+            }
+        });
     }
 }
